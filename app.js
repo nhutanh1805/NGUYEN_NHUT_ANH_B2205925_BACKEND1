d4fed9ed8f690f507f321bc78cdefcd980eafa44
nhutanh1805 <nhutanh1805vlog@gmail.com>
2025-10-17 12:15:02 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error"); 
 
 const contactsRouter = require("./app/routes/contact.route");
 
@@ -14,4 +15,19 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactsRouter);
 
+app.use((req, res, next) => {
+    
+    return next(new ApiError(404, "Resource not found"));
+});
+
+
+app.use((err, req, res, next) => {
+   
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
 module.exports = app;
+
+
